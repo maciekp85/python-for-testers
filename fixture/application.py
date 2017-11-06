79592714cea67a12dea9a358a7bf6cb17f304ad1
Maciej Piotrowski <nishi@wimiip.pl>
2017-11-06 20:17:08 +0100

    Extracted class for working with groups

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
+
 
 class Application:
 
@@ -8,6 +10,7 @@ class Application:
                             firefox_binary="C:/Program Files (x86)/Mozilla Firefox/ESR/firefox.exe")
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
 
     def open_home_page(self):
         wd = self.wd
@@ -17,36 +20,9 @@ class Application:
         wd = self.wd
         wd.find_element_by_link_text("home page").click()
 
-    def return_to_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
-
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
     def open_add_new_contact_page(self):
         wd = self.wd
         wd.find_element_by_link_text("add new").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_groups_page()
-        # init group creation
-        wd.find_element_by_xpath("//div[@id='content']/form/input[4]").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
 
     def create_contact(self, contact):
         wd = self.wd
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -14,12 +14,12 @@ def app(request):
     
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="test name", header="header name", footer="footer name"))
+    app.group.create(Group(name="test name", header="header name", footer="footer name"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
 
