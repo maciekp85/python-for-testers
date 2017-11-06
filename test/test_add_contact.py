f89136b089549eec8a855e97220d07bb2375700a
Maciej Piotrowski <nishi@wimiip.pl>
2017-11-06 19:49:18 +0100

    Created packages for different spheres of responsibility

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import pytest
-from contact import Contact
-from application import Application
+
+from fixture.application import Application
+from model.contact import Contact
 
 
 @pytest.fixture
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import pytest
-from group import Group
-from application import Application
+
+from fixture.application import Application
+from model.group import Group
 
 
 @pytest.fixture
