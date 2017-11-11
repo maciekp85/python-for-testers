6e2958e87607632552450336f773f6299a0b86ba
Maciej Piotrowski <nishi@wimiip.pl>
2017-11-11 20:57:08 +0100

    Created test for editing first group

--- fixture/group.py
+++ fixture/group.py
@@ -38,3 +38,24 @@ class GroupHelper:
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
+
+    def edit_first_group(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit edit button
+        wd.find_element_by_name("edit").click()
+        # fill group form in edition mode
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group edition
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
