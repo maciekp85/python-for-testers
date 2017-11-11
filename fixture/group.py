0414eb2acd1816de984ab54bd51df512413206d1
Maciej Piotrowski <nishi@wimiip.pl>
2017-11-11 20:44:24 +0100

    Created test for deleting group

--- fixture/group.py
+++ fixture/group.py
@@ -29,3 +29,12 @@ class GroupHelper:
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
