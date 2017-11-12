9cccf393841f4472b5398ed28e51607fd6a04921
Maciej Piotrowski <nishi@wimiip.pl>
2017-11-12 22:41:10 +0100

    My homework Lesson 3. Exercise no.8 Checking of fixture validation and session state.
    Testing conditions: if-then-else. Intellectual login.

--- fixture/application.py
+++ fixture/application.py
@@ -9,7 +9,7 @@ class Application:
     def __init__(self):
         self.wd = WebDriver(capabilities={"marionette": False},
                             firefox_binary="C:/Program Files (x86)/Mozilla Firefox/ESR/firefox.exe")
-        self.wd.implicitly_wait(60)
+        self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
--- fixture/session.py
+++ fixture/session.py
@@ -14,6 +14,29 @@ class SessionHelper:
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
+
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//div/div[1]/form/b").text == "(" + username + ")"
+
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
+
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            self.logout()
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("Logout")) > 0
+
