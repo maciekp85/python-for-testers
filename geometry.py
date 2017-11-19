94c426f34d59272c90d40d2c49298aa91e313553
Maciej Piotrowski <nishi@wimiip.pl>
2017-11-19 22:22:45 +0100

    Lambda functions

--- geometry.py
+++ geometry.py
@@ -1,8 +1,22 @@
 from geom2d import *
 
 # Lists sorting
-l1 = [Point(0, 0), Point(1, 2), Point(2, 1)]
+l1 = [Point(3, 1), Point(0, 0), Point(1, 2)]
 
-l2 = sorted(l1)
+
+# def x(p):
+#     return p.x
+
+
+# def y(p):
+#     return p.y
+
+# First method
+# l2 = sorted(l1, key=p.x)
+# l2 = sorted(l1, key=p.y)
+
+# Second method (lambda functions)
+# l2 = sorted(l1, key=lambda p: p.x)
+l2 = sorted(l1, key=lambda p: p.distance(Point(0, 0)))
 print(l1)
 print(l2)
