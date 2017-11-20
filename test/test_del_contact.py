a98f67074d469f96a43bf0657ee88a896214abc1
Maciej Piotrowski <nishi@wimiip.pl>
2017-11-20 20:48:46 +0100

    My homework Lesson 4. Exercise no.11 Verification in the form of comparing lists
    Deleting contacts

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -8,5 +8,10 @@ def test_delete_first_contact(app):
                                home="+12 111 222 333", mobile="111222333", work="777888999", fax="",
                                email="jan.kowalski@test.pl", email2="jan.kowalski2@test.pl", byear="1985",
                                address2="test secondary address", phone2="", notes="test notes"))
+    old_contacts = app.contact.get_contact_list()
     app.contact.delete_first_contact()
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) - 1 == len(new_contacts)
+    old_contacts[0:1] = []
+    assert old_contacts == new_contacts
 
