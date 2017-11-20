158639c78e2e1a5fbbae4eb8820a62d69f12bece
Maciej Piotrowski <nishi@wimiip.pl>
2017-11-20 18:52:02 +0100

    Group lists comparing (adding and modifying groups)

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -5,15 +5,20 @@ def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(name="New group"))
+    group = Group(name="New group")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_modify_group_header(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="test"))
-    old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(header="New header"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+# The test will be parametrized in next lessons group
+# def test_modify_group_header(app):
+#     if app.group.count() == 0:
+#         app.group.create(Group(name="test"))
+#     old_groups = app.group.get_group_list()
+#     app.group.modify_first_group(Group(header="New header"))
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
