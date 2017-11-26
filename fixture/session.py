962a6604cc675e72e8fea0c20314a321e3eaa3a6
Maciej Piotrowski <nishi@wimiip.pl>
2017-11-26 21:52:20 +0100

    String this is a sequence of characters

--- fixture/session.py
+++ fixture/session.py
@@ -12,7 +12,7 @@ class SessionHelper:
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        wd.find_element_by_css_selector('input[type="submit"]').click()
 
     def ensure_login(self, username, password):
         wd = self.app.wd
@@ -25,7 +25,11 @@ class SessionHelper:
 
     def is_logged_in_as(self, username):
         wd = self.app.wd
-        return wd.find_element_by_xpath("//div/div[1]/form/b").text == "(" + username + ")"
+        return self.get_logged_user() == username
+
+    def get_logged_user(self):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//div/div[1]/form/b").text[1:-1]
 
     def logout(self):
         wd = self.app.wd
