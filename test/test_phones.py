e71f505afd3e2ae958d8f0e02a1c73a83b58ca21
Maciej Piotrowski <nishi@wimiip.pl>
2017-11-30 11:57:00 +0100

    Rebuilt phones test on contact view

--- test/test_phones.py
+++ test/test_phones.py
@@ -8,12 +8,9 @@ def test_phones_on_home_page(app):
 
 
 def test_phones_on_contact_view(app):
-    contact_from_view_page = app.contact.get_contact_from_view_page(0)
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_view_page.home == contact_from_edit_page.home
-    assert contact_from_view_page.work == contact_from_edit_page.work
-    assert contact_from_view_page.mobile == contact_from_edit_page.mobile
-    assert contact_from_view_page.phone2 == contact_from_edit_page.phone2
+    contact_from_view_page = app.contact.get_contact_list()[0]
+    contact_from_edit_page = app.contact.get_contact_from_view_page(0)
+    assert contact_from_view_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
 
 def clear(s):
