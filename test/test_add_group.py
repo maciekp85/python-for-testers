286f0c3e5069e3aa71c9d48352fa0904051d4d71
Maciej Piotrowski <nishi@wimiip.pl>
2017-11-30 23:13:22 +0100

    Loading test data from a file.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,18 +1,6 @@
 import pytest
 from model.group import Group
-import random
-import string
-
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Group(name="", header="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-    for i in range(5)
-]
+from data.add_group import constant as testdata
 
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
