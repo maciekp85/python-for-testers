a677872b0031cccea4530354a10b7d71c252d484
Maciej Piotrowski <nishi@wimiip.pl>
2017-12-01 11:35:57 +0100

    Loading test data from a file

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,10 +1,8 @@
-import pytest
 from model.group import Group
-from data.add_group import constant as testdata
 
 
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_add_group(app, group):
+def test_add_group(app, data_groups):
+    group = data_groups
     old_groups = app.group.get_group_list()
     app.group.create(group)
     new_groups = app.group.get_group_list()
