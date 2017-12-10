a0a668c707ec7842a16ded3fe8bbf82d99e0a109
Maciej Piotrowski <nishi@wimiip.pl>
2017-12-10 20:44:17 +0100

    Loading objects from database

--- fixture/db.py
+++ fixture/db.py
@@ -1,5 +1,7 @@
 import pymysql
 
+from model.group import Group
+
 
 class DbFixture:
 
@@ -9,6 +11,18 @@ class DbFixture:
         self.user = user
         self.password = password
         self.connection = pymysql.connect(host=host, database=name, user=user, password=password)
+
+    def get_group_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
+            for row in cursor:
+                (id, name, header, footer) = row
+                list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list
 
     def destroy(self):
         self.connection.close()
