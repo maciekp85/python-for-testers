32989975968487c820803a3c698cf4f23118af1c
Maciej Piotrowski <nishi@wimiip.pl>
2017-12-10 16:23:35 +0100

    Database driver mysql-connector-python-rf

---
+++ check_db_connection.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+connection = mysql.connector.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+
+try:
+    cursor = connection.cursor()
+    cursor.execute("select * from group_list")
+    for row in cursor.fetchall():
+        print(row)
+finally:
+    connection.close()
