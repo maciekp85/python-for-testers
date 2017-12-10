80b7e569fe43bfd93453798c6060b0c93987dcd0
Maciej Piotrowski <nishi@wimiip.pl>
2017-12-10 21:13:28 +0100

    Assertions

--- fixture/db.py
+++ fixture/db.py
@@ -1,4 +1,4 @@
-import pymysql
+import mysql.connector
 
 from model.group import Group
 
@@ -10,7 +10,8 @@ class DbFixture:
         self.name = name
         self.user = user
         self.password = password
-        self.connection = pymysql.connect(host=host, database=name, user=user, password=password)
+        self.connection = mysql.connector.connect(host=host, database=name, user=user, password=password)
+        self.connection.autocommit = True
 
     def get_group_list(self):
         list = []
