276a883da5582fc3c37e228b49a7d6e186d1206d
Maciej Piotrowski <nishi@wimiip.pl>
2017-12-10 16:52:18 +0100

    Database fixture

---
+++ fixture/db.py
@@ -0,0 +1,14 @@
+import pymysql
+
+
+class DbFixture:
+
+    def __init__(self, host, name, user, password):
+        self.host = host
+        self.name = name
+        self.user = user
+        self.password = password
+        self.connection = pymysql.connect(host=host, database=name, user=user, password=password)
+
+    def destroy(self):
+        self.connection.close()
