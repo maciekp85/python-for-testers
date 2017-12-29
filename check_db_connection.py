18df2a76afb6cf2fa7dd9f684709ff62ae4aa632
Maciej Piotrowski <nishi@wimiip.pl>
2017-12-29 20:20:30 +0100

    ORM - Object Relational Mapping

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,11 +1,11 @@
-from fixture.db import DbFixture
+from fixture.orm import ORMFixture
 
-db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-    contacts = db.get_contact_list()
-    for contact in contacts:
-        print(contact)
-    print(len(contacts))
+    l = db.get_contact_list()
+    for item in l:
+        print(item)
+    print(len(l))
 finally:
-    db.destroy()
+    pass #db.destroy()
---
+++ fixture/orm.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+from pony.orm import *
+from model.group import Group
+from model.contact import Contact
+from pymysql.converters import decoders
+
+
+class ORMFixture:
+    db = Database()
+
+    class ORMGroup(db.Entity):
+        _table_ = 'group_list'
+        id = PrimaryKey(int, column='group_id')
+        name = Optional(str, column='group_name')
+        header = Optional(str, column='group_header')
+        footer = Optional(str, column='group_footer')
+
+    class ORMContact(db.Entity):
+        _table_ = 'addressbook'
+        id = PrimaryKey(int, column='id')
+        firstname = Optional(str, column='firstname')
+        lastname = Optional(str, column='lastname')
+        deprecated = Optional(datetime, column='deprecated')
+
+    def __init__(self, host, name, user, password):
+        self.db.bind('mysql', host=host, database=name, user=user, password=password, conv=decoders)
+        self.db.generate_mapping()
+        sql_debug(True)
+
+    def convert_groups_to_model(self, groups):
+        def convert(group):
+            return Group(id=str(group.id), name=group.name, header=group.header, footer=group.footer)
+        return list(map(convert, groups))
+
+    @db_session
+    def get_group_list(self):
+        return self.convert_groups_to_model(list(select(g for g in ORMFixture.ORMGroup)))
+
+    def convert_contacts_to_model(self, contacts):
+        def convert(contact):
+            return Contact(id=str(contact.id), firstname=contact.firstname, lastname=contact.lastname)
+        return list(map(convert, contacts))
+
+    @db_session
+    def get_contact_list(self):
+        return self.convert_contacts_to_model(list(select(c for c in ORMFixture.ORMContact if c.deprecated is None)))
